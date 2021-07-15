059132ed2c5ab1f3b900b3c5248f9d0513df0774
Nikita <frommyhdd@gmail.com>
2021-07-15 12:59:31 +0300

    ADDED STATIC_ROOT

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -105,8 +105,6 @@ DATABASES = {
 #     }
 # }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -144,9 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =  (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS =  (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
