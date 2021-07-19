a93f970540f217b003d62486feed292223dee23d
Nikita <frommyhdd@gmail.com>
2021-07-19 16:02:17 +0300

    added get_links_menu|() function in mainapp.views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import ProductCategory, Product
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+from django.core.cache import cache
+
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.all()
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.all()
+
 
 def index(request):
     context = {
@@ -10,7 +24,8 @@ def index(request):
 
 def products(request, category_id=None, page=1):
     context = {'title': 'Geekshop - каталог',
-                'categories' : ProductCategory.objects.all()}
+               'categories': get_links_menu()}
+                #'categories' : ProductCategory.objects.all()}
     products = Product.objects.filter(category=category_id).select_related('category') if category_id else Product.objects.all()
     paginator = Paginator(products, per_page=3)
     try:
