e1a7e4e022567a8291b9ab8bc5ec09a8dde17b4f
Nikita <frommyhdd@gmail.com>
2021-07-20 13:10:53 +0300

    removed @cached|_property from basketapp

--- basketapp/models.py
+++ basketapp/models.py
@@ -23,21 +23,21 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
-    @cached_property
-    def get_items_cached(self):
-        return self.user.baskets.select_related()
+    # @cached_property
+    # def get_items_cached(self):
+    #     return self.user.baskets.select_related()
 
     def sum(self):
         return self.quantity * self.product.price
 
     def total_quantity(self):
-        #baskets = Basket.objects.filter(user=self.user)
-        baskets = self.get_items_cached
+        baskets = Basket.objects.filter(user=self.user)
+        # baskets = self.get_items_cached
         return sum([basket.quantity for basket in baskets])
 
     def total_sum(self):
-        #baskets = Basket.objects.filter(user=self.user)
-        baskets = self.get_items_cached
+        baskets = Basket.objects.filter(user=self.user)
+        # baskets = self.get_items_cached
         return sum([basket.sum() for basket in baskets])
 
     @staticmethod
