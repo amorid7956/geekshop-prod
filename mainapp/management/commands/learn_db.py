82f04dd48a175311edb27d2d9380d3ed2501cc37
Nikita <frommyhdd@gmail.com>
2021-07-22 12:22:07 +0300

    changed productcategory to category into Product model filter

--- mainapp/management/commands/learn_db.py
+++ mainapp/management/commands/learn_db.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         products_list = Product.objects.filter(
-            Q(productcategory__name='Обувь') |  Q(productcategory__name='Аксессуары')
+            Q(category__name='Обувь') |  Q(category__name='Аксессуары')
         )
 
         print(products_list, sep='\n')
