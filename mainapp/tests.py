e3062468c59e5ba597c216e26a6d1744ec954b55
Nikita <frommyhdd@gmail.com>
2021-07-23 10:26:18 +0300

    added to mainapp/tests.py test_mainapp_urls

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from mainapp.models import Product, ProductCategory
+from django.test.client import Client
+
+class TestMainSmokeTest(TestCase):
+    status_code_success = 200
+
+    def setUp(self):
+        cat_1 = ProductCategory.objects.create(name='cat 1')
+        prod_1 = Product.objects.create(category=cat_1, name='prod 1')
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('main/')
+        self.assertEqual(response.status_code, self.status_code_success)
+
+
 
 # Create your tests here.
